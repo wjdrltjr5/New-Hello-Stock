600bf84f41cb26509f95413238ae0296c166c8d8
Jeong gi seok <wjdrltjr5@naver.com>
2024-10-25 17:12:51 +0900

    init : backend project

---
+++ new-hello-stock-backend/src/main/java/jgs/stock/NewHelloStockBackendApplication.java
@@ -0,0 +1,13 @@
+package jgs.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewHelloStockBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewHelloStockBackendApplication.class, args);
+    }
+
+}
---
+++ new-hello-stock-backend/src/test/java/jgs/stock/NewHelloStockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jgs.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewHelloStockBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
